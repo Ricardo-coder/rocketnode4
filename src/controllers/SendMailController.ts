05b4b7de066e013dad98863e33dda47975e8db3b
Ricardo Freitas <ricardo.freitas@acct.global>
2021-06-07 17:57:22 -0300

    Answer route and controller

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -34,16 +34,18 @@ class SendMailController {
       relations: ['user', 'survey']
     })
 
-    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
     const variables = {
       name: user.name,
       title: survey.title,
       description: survey.description,
-      user_id: user.id,
+      id: '',
       link: process.env.URL_MAIL
     }
 
+    const npsPath = resolve(__dirname, '..', 'views', 'emails', 'npsMail.hbs')
+
     if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id
       await SendMailService.execute(email, survey.title, variables, npsPath)
 
       return response.json(surveyUserAlreadyExists)
@@ -53,6 +55,8 @@ class SendMailController {
       survey_id
     })
     await surveysUsersRepository.save(surveyUser)
+
+    variables.id = surveyUser.id
 
     await SendMailService.execute(email, survey.title, variables, npsPath)
 

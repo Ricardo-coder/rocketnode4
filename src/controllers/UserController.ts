2de6a52a742ad0c659e06f543e884ef1733c8298
Ricardo Freitas <ricardo.freitas@acct.global>
2021-06-07 08:43:10 -0300

    CreateSurveys

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,15 @@
 import { Request, Response } from "express"
-import { getRepository } from 'typeorm'
-import { User } from '../models/User'
+import { getCustomRepository } from 'typeorm'
+import { UsersRepository } from "../repositories/UsersRepository"
 
 class UserController {
-  async create(request:Request, response:Response) {
+  async create(request: Request, response: Response) {
     const { name, email } = request.body
-    const usersRepository = getRepository(User)
+    const usersRepository = getCustomRepository(UsersRepository)
     const userAlreadyExists = await usersRepository.findOne({ email })
     const user = usersRepository.create({ name, email })
 
-    if(userAlreadyExists) {
+    if (userAlreadyExists) {
       return response.status(400).json({ error: "User already exists!" })
     }
 
